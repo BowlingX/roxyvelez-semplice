f4915dad68fcd43cd46ef01554dc45b334055eb1
David Heidrich <me@bowlingx.com>
2015-08-04 00:33:30 +0200

    roxy footer

---
+++ footer.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * footer
+ * semplice.theme
+ */
+?>
+			<!-- content -->
+			<footer class="footer">
+				<p>Copyright 2011 – <?=date('Y')?> Roxy Velez  ·  MADE WITH  ♡</p>
+			</footer>
+			</div>
+		<!-- wrapper -->
+		</div>
+		<div class="to-the-top">
+			<a class="top-button"><?php echo setIcon('arrow_up'); ?></a>
+		</div>
+		<div class="overlay fade"></div>
+		<?php wp_footer(); # include wordpress footer ?>
+		<script type="text/javascript">
+			(function($) {
+				$(document).ready(function () {
+					$(".live-video video, .live-audio audio, .cover-video video, .wysiwyg video, .wysiwyg audio").mediaelementplayer();
+				});
+			})(jQuery);
+		</script>
+	</body>
+</html>
--- partials/fullscreen-cover.php
+++ partials/fullscreen-cover.php
@@ -6,7 +6,6 @@ $detect = new Mobile_Detect;
 // muted video
 $muted = 'muted';
 $vimeoUrl = get_field('vimeo_url');
-
 // is linked to a project?
 if (filter_var(get_field('cover_link'), FILTER_VALIDATE_BOOLEAN) === TRUE) {
     $is_link = true;
@@ -24,34 +23,45 @@ if (get_field('cover_bg_image') && get_field('cover_bg_type') === 'image') {
     $bg_data = 'color';
 }
 
+$backgroundFlag = get_field('video_mute') !== 'no';
+
 // unmute video
 if (get_field('cover_bg_type') === 'video' && get_field('video_mute') === 'no') {
     $muted = false;
 }
 
 if ($is_link) : echo $link; endif;
-echo '<div class="cover-' . get_the_id() . ' fullscreen-cover" data-bg-type="' . $bg_data . '" data-cover-id="' . get_the_id() . '">';
+echo '<div class="cover-' . get_the_id() . ' fullscreen-cover" data-bg-type="' . $bg_data . '
+    " data-cover-id="' . get_the_id() . '">';
 if (get_field('cover_bg_type') === 'image') : ?>
     <div class="cover-image"
-         data-parallax-scrolling="<?php if (get_field('cover_parallax')) : echo get_field('cover_parallax');
+         data-parallax-scrolling="<?php if (get_field('cover_parallax')) :
+             echo get_field('cover_parallax');
          else : echo 'enabled'; endif; ?>" <?php if (get_field('cover_bg_zoom') === 'enabled') {
         echo 'data-image-zoom="zoom"';
-    } ?> <?php if (get_field('cover_bg_image_scale') === 'actual-size') : echo 'data-bg-align="' . get_field('cover_bg_image_align') . '"'; endif; ?>></div>
+    } ?> <?php if (get_field('cover_bg_image_scale') === 'actual-size') :
+        echo 'data-bg-align="' . get_field('cover_bg_image_align') . '"'; endif; ?>></div>
 <?php else : ?>
-        <?php $video_type = get_field('cover_videotype'); ?>
-        <div class="cover-video flex-center cover-video-responsive <?=get_field('video_mute') !== 'no' ? 'just-background' : ''?>" data-has-bg="true">
-            <div class="box-round" data-play-cover="video-modal">
-                <i class="icon-play"></i>
-            </div>
-            <div id="video-modal" class="modal video-modal">
-                <?php if (get_field($video_type . '_mp4')) : ?>
-                    <div class="video-embed-container">
-                        <iframe src="<?php echo get_field($video_type . '_mp4') ?>?api=1"
-                                frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                    </div>
-                <?php endif; ?>
-            </div>
+    <?php $video_type = get_field('cover_videotype'); ?>
+    <div
+        style=" <?= $backgroundFlag? '' : 'background:'.get_field('video_width') ?>"
+        class="cover-video flex-center cover-video-responsive <?= $backgroundFlag ? 'just-background' : '' ?>"
+        data-has-bg="true">
+        <div class="box-round" data-play-cover="video-modal"
+             style="<?= !$backgroundFlag ?
+                 'background:linear-gradient(to right, rgba(255, 249, 240, 0.13), rgba(255, 249, 240, 0.13)), url('.get_field('video_fallback_bg').')' : '' ?>">
+            <i class="icon-play"></i>
         </div>
+        <div id="video-modal" class="modal video-modal">
+            <i data-close-modal class="modal-close icon-cancel"></i>
+            <?php if (get_field($video_type . '_mp4')) : ?>
+                <div class="video-embed-container">
+                    <iframe src="<?php echo get_field($video_type . '_mp4') ?>?api=1"
+                            frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
 <?php endif; ?>
 <?php if (filter_var(get_field('hide_cover_headline'), FILTER_VALIDATE_BOOLEAN) !== TRUE) : ?>
     <?php if (get_field('cover_headline') && trim(get_field('cover_headline')) !== '' || get_field('cover_headline_image')) : ?>
